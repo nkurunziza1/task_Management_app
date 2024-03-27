da7cc5c6ceb1be0169d499fd97b80672d65af41c
nkurunziza1 <alexendre.nkurunziza@gmail.com>
2024-03-27 16:09:54 +0200

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <h1>Task Management system</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
